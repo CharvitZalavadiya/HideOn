788368ce0fa396b50610a476e79c8c3e1b767477
Harshil8025 <hp9157486670@gmail.com>
2025-04-01 15:11:26 +0530

    Made the gui form

--- src/components/gui.jsx
+++ src/components/gui.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import "./gui.css"
 
 export default function Gui() {
   return (
     <div>
-      gui
+      <form >
+      Mode:
+        <input type="radio" name="Mode" value="Encryption" />
+       <label for="Encryption">Encryption</label>
+        <input type="radio" name="Mode"  value="Decryption"/>
+        <label for="Decryption">Decryption</label><br />
+      cipher:
+        <select name="Cipher" >
+            <option value="Caesar">caesar Cipher</option>
+            <option value="Monoalphabetic">Monoalphabetic Cipher</option>
+            <option value="Railfence">Railfence Cipher</option>
+            <option value="Columnar">Columnar Cipher</option>
+            <option value="Doublecolumnar">Doublecolumnar Cipher</option>
+            <option value="Hill">Hill Cipher</option>
+            <option value="Vignere">Vignere Cipher</option>
+            <option value="Vernam">Vernam Cipher</option>
+            <option value="Autokey">Autokey Cipher</option>
+            <option value="Playfair">Playfair Cipher</option>
+            <option value="Fiestel">Fiestel Cipher</option>
+            <option value="AES">AES</option>
+            <option value="DES">DES</option>            
+        </select><br />
+       
+        Message: <textarea rows={5} cols={100} name="textbox" className='gui-textarea' /><br />
+        key: <input type="password" name="key" /><br />
+        
+      </form>
     </div>
   )
 }
